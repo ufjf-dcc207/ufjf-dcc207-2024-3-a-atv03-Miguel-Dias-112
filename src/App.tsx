470a4c83aa5221401bf4142a4e2074243142bc10
MiguelDias <miguel.dias@estudante.ufjf.br>
2024-12-08 11:35:29 -0300

    muda datas

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,12 @@ function App() {
   return (
     <div className='app'>
      
-      <Exibicao inicio={ new Date("2024-11-06T16:00:00.000-03:00")} fim={new Date()} local='aaa'>
+      <Exibicao inicio={ new Date("2024-11-06T16:00:00.000-03:00")} fim={new Date("2024-11-06T16:00:00.000-07:00")} local='aaa'>
         <Animal nome='leao' icone='🦁' peso={10} extinção={false}/>
         <Animal nome='tigre' icone='🐯' peso={0} extinção={true}/>
         <Animal nome='girafa' icone='🦒' peso={0} extinção={false}/>
       </Exibicao>
+      
     </div>
   )
 }
