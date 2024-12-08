e9385f8aa3fb85a430b826d8d68752f50f47753a
MiguelDias <miguel.dias@estudante.ufjf.br>
2024-12-08 11:55:45 -0300

    corrije base

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/ufjf-dcc207-2024-3-a-atv02-Miguel-Dias-112'
+  base:'/ufjf-dcc207-2024-3-a-atv03-Miguel-Dias-112'
 })
